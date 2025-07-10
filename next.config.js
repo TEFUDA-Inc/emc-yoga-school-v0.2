55b1e14400c92e199d7a151a0fcd3dea3a9d26b5
Andy <hidetosaito21a@gmail.com>
2025-07-11 00:06:24 +0900

    ci: 再びGitHub Pagesデプロイ設定を追加

--- next.config.js
+++ next.config.js
@@ -1,10 +1,18 @@
 /** @type {import('next').NextConfig} */
+
+const repo = 'emc-yoga-school-v0.2'
+const assetPrefix = `/${repo}/`
+const basePath = `/${repo}`
+
 const nextConfig = {
-  output: "export",
+  output: 'export',
+  assetPrefix: assetPrefix,
+  basePath: basePath,
   trailingSlash: true,
   images: {
     unoptimized: true,
   },
-};
+  reactStrictMode: true,
+}
 
 module.exports = nextConfig;
