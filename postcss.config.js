e92e216127de0ea1a420ff6c3913d98e82988db8
Andy <hidetosaito21a@gmail.com>
2025-07-11 00:46:46 +0900

    fix: PostCSS/Tailwindの設定を最終統一

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  plugins: {
-    'postcss-nesting': {},
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+  plugins: [
+    require('postcss-nesting'),
+    require('tailwindcss'),
+    require('autoprefixer'),
+  ],
 };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,6 @@ const config: Config = {
     hoverOnlyWhenSupported: true,
   },
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
